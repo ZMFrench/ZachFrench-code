2a80ddd57042f98e750d356fc59deb39e3ff06e0
ZMFrench <inbox@zachfrench.com>
2021-02-16 16:32:40 -0500

    First draft of SuperHero Tour practice application

--- 1-csharp/TourOfHeroes/tohUI/Program.cs
+++ 1-csharp/TourOfHeroes/tohUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tohModels;
 
 namespace tohUI
 {
@@ -6,7 +7,25 @@ namespace tohUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Add Hero method
+            Hero newHero = new Hero();
+            SuperPower newSuperPower = new SuperPower();
+            
+            Console.WriteLine("Enter Hero Name: ");
+            newHero.HeroName = Console.ReadLine();
+            Console.WriteLine("Enter the Hero's HP: ");
+            newHero.HP = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Superpower name: ");
+            newSuperPower.Name = Console.ReadLine();
+            Console.WriteLine("Enter their Superpower's description: ");
+            newSuperPower.Description = Console.ReadLine();            
+            Console.WriteLine("Enter their Superpower's damage: ");
+            newSuperPower.Damage = int.Parse(Console.ReadLine());
+            newHero.SuperPower = newSuperPower;            
+            Console.WriteLine("Set the Hero's element: ");
+            newHero.ElementType = Enum.Parse<Element>(Console.ReadLine());
+
+            Console.WriteLine($"Hero created with the following details:\n\t Name: {newHero.HeroName}\n\t SuperPower: {newHero.SuperPower.Name}");
         }
     }
 }
